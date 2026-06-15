5730239898171e1af7ea6768786f73b7e815c1b9
zhangxuhui8 <joezxh@qq.com>
2026-06-16 00:40:40 +0800

    refactor: 删除旧模块目录(已合并到ontograph-backend)

--- ontograph-module-core/src/main/java/com/ontograph/module/graphiti/model/search/CommunitySearchMethod.java
+++
@@ -1,9 +0,0 @@
-package com.ontograph.module.graphiti.model.search;
-
-/**
- * Community 搜索方法枚举
- */
-public enum CommunitySearchMethod {
-    cosine_similarity,
-    bm25
-}
--- ontograph-module-core/src/main/java/com/ontograph/module/graphiti/model/search/EpisodeSearchMethod.java
+++
@@ -1,9 +0,0 @@
-package com.ontograph.module.graphiti.model.search;
-
-/**
- * Episode 搜索方法枚举
- */
-public enum EpisodeSearchMethod {
-    /** Episode 仅支持 BM25 全文搜索 */
-    bm25
-}
